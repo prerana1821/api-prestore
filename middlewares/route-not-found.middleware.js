52e36155d2cc32e46c619d72766fe973abc7e0fd
Prerana Nawar <>
2021-05-06 16:18:11 +0000

    deleted files

--- middlewares/route-not-found.middleware.js
+++
@@ -1,8 +0,0 @@
-const routeNotFound = (req, res, next) => {
-  res.status(404).json({ 
-    success: false, 
-    message: "The route you're looking for couldn't be found" 
-  })
-}
-
-module.exports = { routeNotFound }
